3e7318c440400a97588e7d7ebedf22e520a6cadf
Aladine <alazaiane@yahoo.fr>
2014-02-21 12:40:57 +0100

    push heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+	def index	
+	end
+
+	def perf_photo
+	end
+
+	def paint
+	end
+
+	def chemin
+	end
+
+	def hijra
+	end
+
+	def achat
+	end
+
+	def bio
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiteApp::Application.config.secret_key_base = 'daea789cfc6b59e17db3bc47cad8d21897d0f0218a8759821f8d14da44c162472c3626ab2b611d386ad96c6b889bf1a6b0c9efde2bbd043b8a3ccf4b030b2842'
